54c03a4c56e24e6c8083a69dd88feaba709ceae1
sonielbr <sonielbr@gmail.com>
2024-01-30 15:55:25 -0300

    soni initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  //if (body.classList.contains("light")) {
+    //html.classList.remove("light")
+  //} else {
+    //html.classList.add("light")
+   html.classList.toggle('light')
+  }
+
